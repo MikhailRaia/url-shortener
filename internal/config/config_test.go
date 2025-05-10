7be49f6360cdaa904bf718881976443ab53938d7
Mikhail Raya <717dok@gmail.com>
2025-05-11 02:32:20 +0300

    [ITER5] - Added env os config

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -8,10 +8,19 @@ import (
 
 func TestNewConfigDefault(t *testing.T) {
 	oldArgs := os.Args
-	defer func() { os.Args = oldArgs }()
+	oldServerAddress := os.Getenv("SERVER_ADDRESS")
+	oldBaseURL := os.Getenv("BASE_URL")
+
+	defer func() {
+		os.Args = oldArgs
+		os.Setenv("SERVER_ADDRESS", oldServerAddress)
+		os.Setenv("BASE_URL", oldBaseURL)
+	}()
+
+	os.Unsetenv("SERVER_ADDRESS")
+	os.Unsetenv("BASE_URL")
 
 	flag.CommandLine = flag.NewFlagSet(os.Args[0], flag.ExitOnError)
-
 	os.Args = []string{"cmd"}
 
 	cfg := NewConfig()
@@ -27,10 +36,19 @@ func TestNewConfigDefault(t *testing.T) {
 
 func TestNewConfigWithArgs(t *testing.T) {
 	oldArgs := os.Args
-	defer func() { os.Args = oldArgs }()
+	oldServerAddress := os.Getenv("SERVER_ADDRESS")
+	oldBaseURL := os.Getenv("BASE_URL")
+
+	defer func() {
+		os.Args = oldArgs
+		os.Setenv("SERVER_ADDRESS", oldServerAddress)
+		os.Setenv("BASE_URL", oldBaseURL)
+	}()
+
+	os.Unsetenv("SERVER_ADDRESS")
+	os.Unsetenv("BASE_URL")
 
 	flag.CommandLine = flag.NewFlagSet(os.Args[0], flag.ExitOnError)
-
 	os.Args = []string{"cmd", "-a", "localhost:8888", "-b", "http://localhost:8000"}
 
 	cfg := NewConfig()
@@ -41,5 +59,61 @@ func TestNewConfigWithArgs(t *testing.T) {
 
 	if cfg.BaseURL != "http://localhost:8000" {
 		t.Errorf("NewConfig() BaseURL = %v, want %v", cfg.BaseURL, "http://localhost:8000")
+	}
+}
+
+func TestNewConfigWithEnv(t *testing.T) {
+	oldArgs := os.Args
+	oldServerAddress := os.Getenv("SERVER_ADDRESS")
+	oldBaseURL := os.Getenv("BASE_URL")
+
+	defer func() {
+		os.Args = oldArgs
+		os.Setenv("SERVER_ADDRESS", oldServerAddress)
+		os.Setenv("BASE_URL", oldBaseURL)
+	}()
+
+	os.Setenv("SERVER_ADDRESS", "localhost:9999")
+	os.Setenv("BASE_URL", "http://localhost:9000")
+
+	flag.CommandLine = flag.NewFlagSet(os.Args[0], flag.ExitOnError)
+	os.Args = []string{"cmd"}
+
+	cfg := NewConfig()
+
+	if cfg.ServerAddress != "localhost:9999" {
+		t.Errorf("NewConfig() ServerAddress = %v, want %v", cfg.ServerAddress, "localhost:9999")
+	}
+
+	if cfg.BaseURL != "http://localhost:9000" {
+		t.Errorf("NewConfig() BaseURL = %v, want %v", cfg.BaseURL, "http://localhost:9000")
+	}
+}
+
+func TestNewConfigEnvOverridesArgs(t *testing.T) {
+	oldArgs := os.Args
+	oldServerAddress := os.Getenv("SERVER_ADDRESS")
+	oldBaseURL := os.Getenv("BASE_URL")
+
+	defer func() {
+		os.Args = oldArgs
+		os.Setenv("SERVER_ADDRESS", oldServerAddress)
+		os.Setenv("BASE_URL", oldBaseURL)
+	}()
+
+	os.Setenv("SERVER_ADDRESS", "localhost:9999")
+	os.Setenv("BASE_URL", "http://localhost:9000")
+
+	flag.CommandLine = flag.NewFlagSet(os.Args[0], flag.ExitOnError)
+	os.Args = []string{"cmd", "-a", "localhost:8888", "-b", "http://localhost:8000"}
+
+	cfg := NewConfig()
+
+	if cfg.ServerAddress != "localhost:9999" {
+		t.Errorf("NewConfig() ServerAddress = %v, want %v", cfg.ServerAddress, "localhost:9999")
+	}
+
+	if cfg.BaseURL != "http://localhost:9000" {
+		t.Errorf("NewConfig() BaseURL = %v, want %v", cfg.BaseURL, "http://localhost:9000")
 	}
 }
