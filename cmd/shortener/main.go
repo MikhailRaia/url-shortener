0e25c4b629309d34d0cc7122cbd599f859466ab2
Mikhail Raya <717dok@gmail.com>
2026-01-14 00:04:22 +0300

    [ITER23] - Added three global variables

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"os"
 	"os/signal"
@@ -13,7 +14,20 @@ import (
 	"github.com/MikhailRaia/url-shortener/internal/config"
 )
 
+var (
+	buildVersion string
+	buildDate    string
+	buildCommit  string
+)
+
 var memprofile = flag.String("memprofile", "", "write memory profile to `file`")
+
+func orNA(s string) string {
+	if s == "" {
+		return "N/A"
+	}
+	return s
+}
 
 func writeHeapProfile(path string) {
 	f, err := os.Create(path)
@@ -25,6 +39,10 @@ func writeHeapProfile(path string) {
 }
 
 func main() {
+	fmt.Printf("Build version: %s\n", orNA(buildVersion))
+	fmt.Printf("Build date: %s\n", orNA(buildDate))
+	fmt.Printf("Build commit: %s\n", orNA(buildCommit))
+
 	if *memprofile != "" {
 		c := make(chan os.Signal, 1)
 		signal.Notify(c, os.Interrupt, syscall.SIGTERM)
